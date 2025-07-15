301c96a334e454bb0a30ed3266fe5378947798fb
KotoFeelGood72 <kotenkolove@yandex.ru>
2025-07-15 11:26:58 +0300

    Адаптировал страницу приложения

--- src/views/ContentPages/ApplicationScreen.vue
+++ src/views/ContentPages/ApplicationScreen.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="container flex items-center lg:gap-[60px]">
-    <div class="max-w-[518px] flex-col flex lg:gap-10">
-      <h1 class="lg:text-40 text-dark font-semibold">Приложение</h1>
-      <p>
+  <div
+    class="container flex items-center lg:gap-[60px] lg:flex-row flex-col lg:text-left text-center"
+  >
+    <div class="max-w-[518px] flex-col flex lg:gap-10 gap-5">
+      <h1 class="lg:text-40 text-dark font-semibold text-22">Приложение</h1>
+      <p class="lg:text-16 text-14">
         Официальное приложение магазина на русском языке. Не нужно скачивать – работает
         прямо внутри Telegram
       </p>
       <a
-        class="inline-flex items-center justify-center gap-6 py-5 px-10 bg-dark rounded-md max-w-[216px] cursor-pointer"
+        class="inline-flex items-center justify-center lg:gap-6 gap-4 lg:py-5 lg:px-10 px-8 py-3 bg-dark rounded-md lg:max-w-[216px] max-w-max cursor-pointer max-lg:mx-auto max-lg:mb-10"
       >
-        <p class="text-18 font-semibold text-white">Перейти</p>
-        <div class="flex items-center justify-center text-white">
+        <p class="lg:text-18 font-semibold text-white text-12">Перейти</p>
+        <div
+          class="flex items-center justify-center text-white lg:w-[34px] lg:h-[34px] w-4 h-4"
+        >
           <Icons icon="icon-park-outline:telegram" color="white" />
         </div>
       </a>
